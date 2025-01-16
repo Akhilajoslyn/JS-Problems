665313e38629a20a40d11befa9822350c8dfe26d
Akhilajoslyn <akhilajoslyn54@gmail.com>
2025-01-16 20:50:16 +0530

    find typeof() for the given data

---
+++ findtypeof.js
@@ -0,0 +1,16 @@
+function getDataType(input) {
+    return typeof input;
+}
+
+// Example 
+let example1 = 42;
+let example2 = "hello";
+let example3 = true;
+let example4 = {name: "Akhila", age: 22};
+let example5 = [1, 2, 3, 4, 5];
+
+console.log(getDataType(example1)); 
+console.log(getDataType(example2)); 
+console.log(getDataType(example3)); 
+console.log(getDataType(example4)); 
+console.log(getDataType(example5)); 
