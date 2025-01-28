4fe99a0fafde7d106eab3601444e35950b599098
Akhilajoslyn <akhilajoslyn54@gmail.com>
2025-01-28 20:33:47 +0530

    function to convert a given number into its opposite sign

---
+++ oppositeSign.js
@@ -0,0 +1,10 @@
+function oppositeSign(num) {
+    return -num;
+}
+
+// Example usage:
+let positiveNum = 42;
+let negativeNum = -15;
+
+console.log(`Opposite of ${positiveNum} is:`, oppositeSign(positiveNum)); // Output: Opposite of 42 is: -42
+console.log(`Opposite of ${negativeNum} is:`, oppositeSign(negativeNum)); // Output: Opposite of -15 is: 15
